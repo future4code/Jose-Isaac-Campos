e74c86d053a792b6251eae0a66b71e4e5ba27638
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-22 13:24:52 -0300

    Finalizado o exercício de escrita de código - 1

--- semana3/variaveis/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -9,3 +9,34 @@
  *  10 10 10
  */
 
+/**
+ * Exercício de escrita de código
+ */
+
+// 1) a, b
+let nome, idade
+
+// 1) c
+console.log('typeof nome: ', typeof nome, '\ntypeof idade: ', typeof idade)
+
+/**
+ * 1) d - resposta
+ *  As variaveis foram criadas mais não foi atribuido nenhum valor a elas
+ */
+
+// 1) e
+nome = prompt('Olá, qual o seu nome?')
+idade = prompt('Prazer ' + nome + ',quantos anos você tem?')
+
+// 1) f
+console.log('typeof nome: ', typeof nome, '\ntypeof idade: ', typeof idade)
+
+/**
+ * 1) f - resposta
+ *  Como agora as variaveis possuem um valor atribuido a elas o que é impresso
+ * é o tipo do dados que eles possuem
+ */
+
+// 1) g
+console.log('Olá ', nome, ', você tem ', idade, ' anos.');
+
