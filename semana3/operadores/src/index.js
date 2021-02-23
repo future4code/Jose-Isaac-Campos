7cef6b0532811e00eea3e62f5322ecd277a37b46
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-23 15:35:27 -0300

    Finalizado o desafio de escrita de código - 3

--- semana3/operadores/src/index.js
+++ semana3/operadores/src/index.js
@@ -82,9 +82,9 @@ console.log('O e-mail ', emailDoUsuario, ' foi cadastrado com sucesso. Seja bem-
  */
 console.log('\n\nDesafio de escrita de código - 1\n\n')
 
-console.log('77°F equivale á ', (77 - 32) * 5 / 9 + 273.15, '°K') // a.
+console.log('77°F equivalem á ', (77 - 32) * 5 / 9 + 273.15, '°K') // a.
 
-console.log('80°C equivale á ', 80 * 9 / 5 + 32, '°F') // b.
+console.log('80°C equivalem á ', 80 * 9 / 5 + 32, '°F') // b.
 
 console.log(
   '30°C equivale á ', 30 * 9 / 5 + 32, '°F',
@@ -95,8 +95,8 @@ let valorEmGrausCelcius
 valorEmGrausCelcius = Number(prompt('Digete um valor em graus Celcius: '))
 
 console.log(
-  valorEmGrausCelcius + '°C equivale á ', valorEmGrausCelcius * 9 / 5 + 32, '°F',
-  '\n' + valorEmGrausCelcius + '°C equivale á ', valorEmGrausCelcius + 273.15, '°K')
+  valorEmGrausCelcius + '°C equivalem á ', valorEmGrausCelcius * 9 / 5 + 32, '°F',
+  '\n' + valorEmGrausCelcius + '°C equivalem á ', valorEmGrausCelcius + 273.15, '°K')
 
 /**
  * Desafio de escrita de código - 2
@@ -110,6 +110,22 @@ quantidadeQuilowattHoraConsumida = Number(prompt('Digite a quantidade de Quilowa
 porcentagemDesconto = Number(prompt('Digite a porcentagem de desconto para a residência:'))
 
 let valor = quantidadeQuilowattHoraConsumida * valorQuilowattHora
-console.log('Total a ser pago (sem o desconto): R$: ', valor)
+console.log('Total a ser pago (sem o desconto): R$: ', valor) // a.
 
-console.log('Total a ser pago (com o desconto de ', porcentagemDesconto, '%): R$: ', valor - valor * (porcentagemDesconto / 100))
+console.log('Total a ser pago (com o desconto de ', porcentagemDesconto, '%): R$: ', valor - (valor * (porcentagemDesconto / 100))) //b.
+
+/**
+ * Desafio de escrita de código - 3
+ */
+console.log('\n\nDesafio de escrita de código - 3\n\n')
+
+console.log('20lb equivalem á ', 20 * 0.4536, 'kg') // a. 9.072kg
+console.log('10.5oz equivalem á ', 10.5 * 0.0283, 'kg') // b. 0.2971kg
+console.log('100mi equivalem á ', 100 * 1609.344, 'm') // c. 160934.4m
+console.log('50ft equivalem á ', 50 * 0.3048, 'm') // d. 15.24m
+console.log('103.56gal equivalem á ', 103.56 / 0.26417, 'l') // e. 392.02l
+console.log('450xic equivalem á ', 450 * 0.24, 'l') // f. 108l
+
+// g.
+let valorEmGal = Number(prompt('Digite a o valor em galões que sera convertido para litros:'))
+console.log(valorEmGal + 'gal equivalem á ', valorEmGal / 0.26417, 'l')
