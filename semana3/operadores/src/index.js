b58cba78c30661a3084ddd65156c1406ceb1f8d2
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-23 13:52:50 -0300

    Finalizado o exercício de interpretação de código

---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,18 @@
+/**
+ * Exercícios de interpretação de código
+ *
+ * 1)
+ * a. false
+ * b. false
+ * c. true
+ * d. boolean
+ *
+ * 2)
+ * a. undefined
+ * b. null
+ * c. 11
+ * d. 3
+ * e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+ * f. 9
+ */
+
