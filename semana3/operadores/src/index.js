bd2add56bc0ff781ffa7660fe33e5e173dd4e6ff
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-23 14:01:08 -0300

    Finalizado o exercício de escrita de código - 1

--- semana3/operadores/src/index.js
+++ semana3/operadores/src/index.js
@@ -16,3 +16,16 @@
  * f. 9
  */
 
+/**
+ * Exercícios de escrita de código - 1
+ */
+
+let idade, melhorAmigoIdade
+
+idade = Number(prompt("Digite sua idade:")) // a.
+melhorAmigoIdade = Number(prompt("Digite a idade do seu ou sua melhor amigo(a):")) // b.
+
+console.log('"Sua idade é maior do que a do seu melhor amigo?', idade > melhorAmigoIdade) // c.
+
+console.log('Vocês tem ', idade - melhorAmigoIdade, ' anos de diferênça de idade entre vocês') //d.
+
