3552025dc0ddcf4054a825ba996416e8cec264c1
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-24 13:48:49 -0300

    Finalizado os exercícios de escrita de código

---
+++ semana3/condicionais/src/index.js
@@ -0,0 +1,43 @@
+/**
+ * Exercícios de interpretação de código
+ *
+ * 1) O programa solicita ao usuário um número e com base nesse
+ * número faz o teste se o mesmo é par ou ímpar.
+ *
+ * Caso seja par exibe a mensagem 'Passou no teste.' e caso seja
+ * ímpar exibe 'Não passou no teste.'
+ *
+ *
+ * 2) a. O código recebe o nome de um fruta e de acordo com a fruta
+ * digitada pelo usuário atribue um valor a variável preço e exibe
+ * em tela o valor para a fruta informada.
+ *
+ * b. 'O preço da fruta Maça é R$ 2.25'
+ *
+ * c. 'O preço da fruta Pêra é R$ 5'
+ *
+ *
+ * 3) a. Esta solicitando a entra de uma número para o usuário
+ * atraves do comando prompt, o retorno do prompt é passado para o
+ * Number como um parâmentro, o number converte essa string em número,
+ * o retorno de Number é atribuido a constante numero.
+ *
+ * b. Se o usuário digitasse 10 seria impresso 'Esse número passou no teste'
+ * e logo após a mensagem seria impresso um erro, se ele digitasse -10 seria
+ * impresso apenas o erro
+ *
+ * c. Sim, como a variável 'mensagem' é filha do 'if' e como o 'console.log'
+ * que está solicitando está um escopo acima (global) ele não consegue ter acesso
+ * a variável e imprime no console que a variável informada para o console não
+ * foi definida, pois para ele a variável não existe no seu escopo.
+ */
+
+
+/**
+ * Exercícios de escrita de código
+ */
+
+/**
+ * EXERCÍCIO 4
+ */
+
