c05f577a71b54e4f57ea096907b63393a1a50d67
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-02-25 13:45:27 -0300

    Finalizado os exercícios de interpretação de código

---
+++ semana3/loops/src/index.js
@@ -0,0 +1,25 @@
+/**
+ * Exercícios de interpretação de código
+ *
+ * 1) A variável 'valor' está sendo incrementada com o valor dela mais
+ * o valor de i a cada interação do for.
+ * -> será exibido o valor 10
+ *
+ * 2) a. será exibido os número 19, 21, 23, 25, 27, 30 cada um em
+ * uma linha diferente.
+ *
+ * b. sim, teriamos que ter uma variável declarada antes do for com ela
+ * iniciando em 0 e detro do for na ultima linha do seu escopo teriamos
+ * que ir incrementando ela de modo a aumentar uma unidade a cada interação
+ * do for.
+ *
+ * DESAFIO 1
+ *
+ * seria impresso quatro linha em ordem cressente, onde a cade linha
+ * seria adicionado um '0' a mais:
+ *
+ * linha 1 -> 0
+ * linha 2 -> 00
+ * linha 3 -> 000
+ * linha 4 -> 0000
+ */
