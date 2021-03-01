56c0c714d03384357d8465f16e5b878a28477532
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-01 15:51:37 -0300

    Finalizado os desafios de escrita de código + correções passagem dos params para as funcções

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -30,21 +30,24 @@
 /**
  * Exercícios de escrita de código
  */
-console.log('Exercícios de escrita de código')
+console.log('\n\nExercícios de escrita de código')
 
 // EXERCÍCIO 4
 console.log('\n\nEXERCÍCIO 4\n')
 
 
 //4) a)
+console.log('\n\nEXERCÍCIO 4) a. :')
 function aboutMe() {
   console.log('Eu sou Isaac, tenho 23 anos, moro na Paraíba e sou estudante.')
 }
 
-console.log('\n\nEXERCÍCIO 4) a. :', aboutMe())
+aboutMe()
 
 
 // 4) b)
+console.log('\n\nEXERCÍCIO 4) b. :')
+
 function about(name, age, city, isStudent) {
   let phrase = ''
 
@@ -53,7 +56,7 @@ function about(name, age, city, isStudent) {
   console.log(`Eu sou ${name}, tenho ${age} anos, moro em ${city} e ${phrase} estudante`)
 }
 
-console.log('\n\nEXERCÍCIO 4) b. :', about())
+about('Isaac', 22, 'Pedra Branca - PB', true)
 
 
 // EXERCÍCIO 5
@@ -65,7 +68,7 @@ function sum(numA, numB) {
   return numA + numB
 }
 
-console.log('\n\nEXERCÍCIO 5) a. :', sum())
+console.log('EXERCÍCIO 5) a. 4 + 10 :', sum(4, 10))
 
 
 // 5) b)
@@ -73,7 +76,7 @@ function greaterOrEqual(numA, numB) {
   if (numA >= numB) { return true } else { return false }
 }
 
-console.log('\n\nEXERCÍCIO 5) b. 10 >= 8:', greaterOrEqual(10, 8))
+console.log('EXERCÍCIO 5) b. 10 >= 8:', greaterOrEqual(10, 8))
 
 
 // 5) c)
@@ -84,7 +87,7 @@ function repeatPhrase(phrase) {
   }
 }
 
-repeatPhrase()
+repeatPhrase('test test test test')
 
 
 // EXERCÍCIO 6
@@ -134,3 +137,92 @@ function amountOfEvenNumbers(array) {
 }
 
 console.log('EXERCÍCIO 6) d. quantidade de números pares: ', amountOfEvenNumbers(array))
+
+
+/**
+ * Desafio de escrita de código
+ */
+console.log('\n\nDesafio de escrita de código')
+
+
+// DESAFIO 1
+console.log('\n\nDESAFIO 1\n')
+
+// 1) 1.
+const print = (string) => { console.log(string) }
+
+// 1) 2.
+const sumNumber = (numA, numB) => {
+  print('DESAFIO 1) ' + (numA + numB))
+}
+
+sumNumber(10, 20)
+
+
+// DESAFIO 2
+console.log('\n\nDESAFIO 2\n')
+
+const numbers = [0, 8, 23, 16, 10, 15, 41, 12, 13]
+
+// 2) a. 
+const pairsSquared = (numbers) => {
+  const pairsNumerous = []
+
+  for (let num of numbers) {
+    if (isNumberEven(num)) { pairsNumerous.push(num * 2) }
+  }
+
+  return pairsNumerous
+}
+
+console.log('DESAFIO 2) a. ', pairsSquared(numbers))
+
+
+// 2) b.
+const largestNumber = (numbers) => {
+  let num = 0
+
+  for (let number of numbers) {
+    if (number > num) { num = number }
+  }
+
+  return num
+}
+
+console.log('DESAFIO 2) b. ', largestNumber(numbers))
+
+
+// 2) c.
+const indexOfLargestNumber = (numbers) => {
+  const num = largestNumber(numbers)
+
+  return numbers.indexOf(num)
+}
+
+console.log('DESAFIO 2) c. index: ', indexOfLargestNumber(numbers))
+
+
+// 2) d.
+const reverse = (numbers) => {
+  let length = numbers.length - 1
+
+  let stopNumber = 0
+  if (numbers.length % 2 === 0) {
+    stopNumber = numbers.length / 2
+  } else {
+    stopNumber = numbers.length / 2 + 1
+  }
+
+  let i = 0
+  while (i <= stopNumber) {
+    let temp = numbers[i]
+
+    numbers[i] = numbers[length - i]
+    numbers[length - i] = temp
+
+    i++
+  }
+  return numbers
+}
+
+console.log('DESAFIO 2) d. ', reverse(numbers))
