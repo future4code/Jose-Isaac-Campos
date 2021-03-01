6f570ec28e06cf741a8b56c21ed1168daa45ad0d
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-01 14:11:25 -0300

    Finalizado os exercicios de interpretação de código

---
+++ semana4/funcoes/src/index.js
@@ -0,0 +1,28 @@
+/**
+ * Exercícios de interpretação de código
+ *
+ * 1)
+ * a. 10
+ * a. 50
+ *
+ * b. A função continuaria funcionando conforme o esperado,
+ * poreém não seria impresso nada no console, pois não estariamos
+ * o utilizando o console.log
+ *
+ *
+ * 2) a. Como o for que está dentro da função so vai até 1, os valores
+ * impressos no console seria:
+ *   Darvas
+ *   Caio
+ *
+ * b. Seria impresso:
+ *  Amanda
+ *  Caio
+ *
+ *
+ * 3) Ela recebe um array e filtra apenas os números pares, e adiciona
+ * eles em um novo array multiplicando o seu valor por ele mesmo. Um nome
+ * mais discritivo para a função poderia ser parAoQuadrado
+ *
+ * */
+
