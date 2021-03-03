9c5717c6ee2984e63e8d2690d8f072508a00bfbd
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-03 16:49:49 -0300

    Concluido o exercício da aula 16

---
+++ semana4/objetos/blog/src/index.js
@@ -0,0 +1,97 @@
+const posts = []
+
+function getPostData(inputFields) {
+  const post = {
+    "title-post": '',
+    "author-post": '',
+    "content-post": '',
+    "image-post": ' '
+  }
+
+  for (let id of inputFields) {
+    const input = document.getElementById(id)
+
+    post[id] = input.value
+    input.value = ''
+  }
+
+  return post
+}
+
+function isValidInput(element) {
+  if (element.value === '') {
+    element.classList.remove('success')
+    element.classList.add('error')
+  } else {
+    element.classList.add('success')
+  }
+}
+
+function isValidData(inputFields) {
+  let isValid = true
+
+  for (let id of inputFields) {
+    let element = document.getElementById(id)
+
+    if (element.value === '') {
+      if (id === 'image-post') {
+
+      } else {
+        element.classList.remove('success')
+        element.classList.add('error')
+        isValid = false
+      }
+    } else {
+      element.classList.remove('success')
+    }
+  }
+
+  if (!isValid) { return false } else { return true }
+}
+
+function isValidImage(element) {
+  if (element.value !== '') {
+    let { value } = element
+    if (!(value.includes('http') &&
+      (value.includes('.jpg') || value.includes('.png'))
+    )) {
+      element.value = ''
+      alert('Imagem inválida')
+    }
+  }
+}
+
+function renderPost() {
+  // sessionStorage.setItem('posts', [...posts])
+
+  // location.pathname = `/blog/src/post.html`
+
+  let container = document.getElementById('container-de-posts')
+
+  container.innerHTML = ''
+  for (const post of posts) {
+    container.innerHTML += `
+      <article>
+        <h3>${post['title-post']}</h3>
+        <p>${post['author-post']}</p>
+        <div>
+          <img id='banner' src='${post['image-post']}'>
+          <p>${post['content-post']}</p>
+        </div>
+      </article>
+    `
+  }
+
+}
+
+function createPost() {
+  const inputFields = ['title-post', 'author-post', 'content-post', 'image-post']
+
+  if (isValidData(inputFields)) {
+    posts.push(getPostData(inputFields))
+    renderPost()
+    alert('Post criado com sucesso!')
+  } else {
+    alert("Por favor, verifique os campos em vermelho")
+  }
+}
