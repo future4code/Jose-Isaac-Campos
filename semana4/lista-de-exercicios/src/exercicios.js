6b2cef2ec2552d28a05ec433f4b046d0f65e6902
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-05 15:19:06 -0300

    Finalizado o exercício 14

--- semana4/lista-de-exercicios/src/exercicios.js
+++ semana4/lista-de-exercicios/src/exercicios.js
@@ -188,6 +188,14 @@ function imprimeChamada() {
 
 function criaRetangulo(lado1, lado2) {
    // implemente sua lógica aqui
+   let retangulo = {
+      largura: lado1,
+      altura: lado2,
+      perimetro: 2 * (lado1 + lado2),
+      area: lado1 * lado2
+   }
+
+   return retangulo
 }
 
 // Exercício 15
