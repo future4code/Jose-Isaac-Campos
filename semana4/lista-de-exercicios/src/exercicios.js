1ca41ee3c6282a80554ee15eaf743c94c5483a3d
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-05 13:49:24 -0300

    Finalizado o exercício 6

--- semana4/lista-de-exercicios/src/exercicios.js
+++ semana4/lista-de-exercicios/src/exercicios.js
@@ -71,6 +71,20 @@ function retornaQuantidadeElementos(array) {
 
 function retornaExpressoesBooleanas() {
    // implemente sua lógica aqui
+   const booleano1 = true
+   const booleano2 = false
+   const booleano3 = !booleano2
+   const booleano4 = !booleano3
+
+   const resposta = [
+      booleano1 && booleano2 && !booleano4,
+      (booleano1 && booleano2) || !booleano3,
+      (booleano2 || booleano3) && (booleano4 || booleano1),
+      !(booleano2 && booleano3) || !(booleano1 && booleano3),
+      !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+   ]
+
+   return resposta
 }
 
 //Exercício 7
