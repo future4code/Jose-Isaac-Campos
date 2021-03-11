40102ea9779b6208f7daca2ecbf7732635bc2bd5
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-03-11 17:16:30 -0300

    Commit de entrega do exercício

--- semana5/aula21/src/App.js
+++ semana5/aula21/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components'
 
 class App extends React.Component {
   state = {
@@ -20,10 +21,71 @@ class App extends React.Component {
         fotoUsuario: 'https://picsum.photos/10/60',
         fotoPost: 'https://picsum.photos/201/150'
       }
-    ]
+    ],
+    criandoPost: false,
+    inputNomeUsuario: '',
+    inputFotoUsuario: '',
+    inputFotoPost: ''
+  }
+
+  handleNomeUsuario = (event) => {
+    this.setState({inputNomeUsuario: event.target.value})
+  }
+
+  handleFotoPost = (event) => {
+    this.setState({inputFotoPost : event.target.value})
+  }
+
+  handleFotoUsuario = (event) => {
+    this.setState({inputFotoUsuario : event.target.value})
+  }
+
+  onClickNovoPost = () => {
+    this.setState({criandoPost: true})
   }
 
   render() {
+    const SecaoCriarPost = styled.div`
+      display: flex;
+      justify-content: stretch;
+      align-items: space-around;
+      margin: 10px 0;
+      width: 302px;
+    `
+
+    const ButaoNovoPost = styled.button`
+      flex: 1;
+      background-color: gray;
+      color: white;
+      font-size: 1.4em;
+    `
+
+    const FormNovoPost = styled.div`
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: space-around;
+      width: 100%;
+      padding: 0 10px;
+    `
+
+    const Input = styled.input`
+      width: 100%;
+      margin-top: 4px;
+    `
+
+    let content
+
+    if (!this.state.criandoPost) {
+      content = <ButaoNovoPost onClick={this.onClickNovoPost}>+</ButaoNovoPost>
+    } else {
+      <FormNovoPost>
+        <Input value={this.state.inputNomeUsuario} onChange={this.handleNomeUsuario} placeholder='Nome do usuário' />
+        <Input value={this.state.inputFotoPost} onChange={this.handleFotoPost} placeholder='Url da image do post' />
+        <Input value={this.state.inputFotoUsuario} onChange={this.handleFotoUsuario} placeholder='Url da image do perfil' />
+      </FormNovoPost>
+    }
+
     const postsComponent = this.state.posts.map((post, index) => {
       return <Post key={index}
         nomeUsuario={post.nomeUsuario}
@@ -34,6 +96,7 @@ class App extends React.Component {
 
     return (
       <div className={'app-container'}>
+        <SecaoCriarPost>{content}</SecaoCriarPost>
         {postsComponent}
       </div>
     );
