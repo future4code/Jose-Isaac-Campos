45281026587ef9183eafaf86414da6eef7793cb7
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-04-16 09:14:34 -0300

    Adicionado validação no form da aplicação a uma viagem

--- semana10/labe-x/src/components/ApplicationForm/ApplicationForm.js
+++ semana10/labe-x/src/components/ApplicationForm/ApplicationForm.js
@@ -1,48 +1,123 @@
-import React, { useState } from 'react'
-import { useRequestData } from '../../hooks/useRequestData'
-import { useForm } from '../../hooks/useForm'
+import React, { useState } from "react";
+import { useRequestData } from "../../hooks/useRequestData";
+import { useForm } from "../../hooks/useForm";
 
-import TitlePage from '../TitlePage/TitlePage'
-import ButtonAction from '../ButtonAction/ButtonAction'
-import { Main, Form, Input, Select, Option } from './applicationForm-style'
+import TitlePage from "../TitlePage/TitlePage";
+import ButtonAction from "../ButtonAction/ButtonAction";
+import { Main, Form, Input, Select, Option } from "./applicationForm-style";
 
-import { countriesName } from '../../constants/countriesNameList'
+import { countriesName } from "../../constants/countriesNameList";
+import { apiSendData } from "../../services/api";
 
 export default function ApplicationForm() {
-    const initialForm = { tripName: '', userName: '', age: '', applicationText: '', profession: '', countryName: '' }
-    const [form, handleForm, resetForm] = useState(initialForm)
-    const [trips, setTrips] = useState([])
+  const initialForm = {
+    tripId: "",
+    userName: "",
+    age: "",
+    applicationText: "",
+    profession: "",
+    countryName: "",
+  };
+  const [form, onChange, resetForm] = useForm(initialForm);
+  const [trips, setTrips] = useState([]);
 
-    useRequestData('/trips', {})
-        .then(response => {
-            setTrips(response.trips)
-        })
+  useRequestData("/trips", {}).then((response) => {
+    setTrips(response.trips);
+  });
 
-    return (
-        <Main>
-            <TitlePage>Comprar um bilhete</TitlePage>
-            <Form>
-                <Select>
-                    <Option>Escolha uma viagem:</Option>
-                    {trips && trips.map(trip => {
-                        return <Option>{trip.name}</Option>
-                    })}
-                </Select>
+  const handleForm = (event) => {
+    event.preventDefault();
+    onSubmit();
+  };
 
-                <Input placeholder='Nome' />
-                <Input placeholder='Idade' />
-                <Input placeholder='Texto de candidatura' />
-                <Input placeholder='Profissão' />
+  const onSubmit = () => {
+    const body = {
+      name: form.userName,
+      age: form.age,
+      applicationText: form.applicationText,
+      profession: form.profession,
+      country: form.countryName,
+    }
 
-                <Select>
-                    <Option>Escolha um país:</Option>
-                    {countriesName.map(country => {
-                        return <Option>{country.nome}</Option>
-                    })}
-                </Select>
+    apiSendData(`/trips/${form.tripId}/apply`, body)
+      .then((apply) => {
+        if (apply.success) {
+            resetForm()
+            alert(apply.message)
+        } else {
+            alert(apply.error)
+        }
+      })
+  }
 
-                <ButtonAction>Enviar</ButtonAction>
-            </Form>
-        </Main>
-    )
+  return (
+    <Main>
+      <TitlePage>Comprar um bilhete</TitlePage>
+      <Form onSubmit={handleForm}>
+        <Select
+          pattern={"[a-zA-Z].{1,}$"}
+          required
+          name="tripId"
+          onChange={onChange}
+        >
+          <Option value="">Escolha uma viagem:</Option>
+          {trips &&
+            trips.map((trip) => {
+              return <Option value={trip.name}>{trip.name} - {trip.planet}</Option>;
+            })}
+        </Select>
+
+        <Input
+          pattern={"[a-zA-Z].{5,}$"}
+          required
+          type="text"
+          onChange={onChange}
+          value={form.userName}
+          name="userName"
+          placeholder="Nome"
+        />
+        <Input
+          required
+          type="number"
+          onChange={onChange}
+          value={form.age}
+          name="age"
+          min="18"
+          placeholder="Idade"
+        />
+        <Input
+          pattern={"[a-zA-Z].{30,}$"}
+          required
+          type="text"
+          onChange={onChange}
+          value={form.applicationText}
+          name="applicationText"
+          placeholder="Texto de candidatura"
+        />
+        <Input
+          pattern={"[a-zA-Z].{10,}$"}
+          required
+          type="text"
+          onChange={onChange}
+          value={form.profession}
+          name="profession"
+          placeholder="Profissão"
+        />
+
+        <Select
+          pattern={"[a-zA-Z].{1,}$"}
+          required
+          name="countryName"
+          onChange={onChange}
+        >
+          <Option value="">Escolha um país:</Option>
+          {countriesName.map((country) => {
+            return <Option value={country.name}>{country.nome}</Option>;
+          })}
+        </Select>
+
+        <ButtonAction>Enviar</ButtonAction>
+      </Form>
+    </Main>
+  );
 }
