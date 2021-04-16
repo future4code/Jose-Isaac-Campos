0752d0d4681b8ab64a2b46b75ce9366480f9e023
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-04-16 11:27:07 -0300

    Criado a página admin home page

--- semana10/labe-x/src/pages/AdminHomePage.js
+++ semana10/labe-x/src/pages/AdminHomePage.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { useProtectedPage } from '../hooks/useProtectedPage'
+import { useHistory } from 'react-router-dom'
+
+import Header from '../components/Header/Header'
+import Nav from '../components/Nav/Nav'
+import ButtonAction from '../components/ButtonAction/ButtonAction'
+import AdminListTripName from '../components/AdminListTripName/AdminListTripName'
+
+import BackIcon from '../icons/undo.png'
+import BuyTicketIcon from '../icons/login.png'
+
+import { goToLastPage, goToApllicationFormPage } from '../router/coordinator'
+
 export default function AdminHomePage() {
-    useProtectedPage()
+    const history = useHistory()
+
+    const menuOptions = [
+        <ButtonAction onClick={() => goToLastPage(history)}>
+            <img src={BackIcon} alt='back'/>
+            Voltar
+        </ButtonAction>,
+        <ButtonAction onClick={() => goToApllicationFormPage(history)}>
+            Logout
+            <img src={BuyTicketIcon} alt='back'/>
+        </ButtonAction>
+    ]
+
     return (
         <div>
-            <p>AdminHomePage</p>
+            <Header />
+            <Nav>{menuOptions}</Nav>
+            <AdminListTripName />
         </div>
     )
 }
