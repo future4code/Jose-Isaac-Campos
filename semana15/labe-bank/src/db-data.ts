2bf703b18d67105b78fffbbc08ce4d8a2da05fee
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-05-21 14:36:33 -0300

    Concluído os requisitos mínimos do projeto

--- semana15/labe-bank/src/db-data.ts
+++ semana15/labe-bank/src/db-data.ts
@@ -4,7 +4,7 @@ export const clients: client[] = [
     {
         name: 'Isaac',
         age: 22,
-        cpf: '111.000.111-00',
+        cpf: '11100011100',
         birthDate: '31/10/98',
         account: {
             balance: 0,
--- semana15/labe-bank/src/index.ts
+++ semana15/labe-bank/src/index.ts
@@ -7,13 +7,38 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/clients', (req: Request, res: Response) => {
+    res.status(200).send(clients)
+})
+
 app.post('/clients', (req: Request, res: Response) => {
     try {
         const newClient: client = req.body.client
 
+        if (!newClient) {
+            throw new Error(`the request body came empty`)
+        }
+
+        if (isNaN(Number(newClient.age)) || !newClient.age) {
+            throw new Error(`age is not a number`)
+        }
+
+        newClient.age = Number(newClient.age)
+
+        if (newClient.age < 18) {
+            throw new Error(`only adults can open a new account`)
+        }
+
+        const cpfIsInUse = clients.find(client => client.cpf === newClient.cpf)
+
+        if (cpfIsInUse) {
+            throw new Error(`The supplied cpf is already in use`)
+        }
+
         console.log(newClient);
 
-        res.status(200).send()
+        clients.push(newClient)
+        res.status(200).send({message: 'Success', client: newClient})
     } catch (error) {
         res.status(400).send({ message: error.message})   
     }
