ddd21a22e48919fe0ad67bad62df30e6d7510e84
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-05-31 15:04:39 -0300

    concluído o exercício 01

---
+++ semana17/aula49/src/controllers/userController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express"
+import { userModel } from '../model/userModel'
+
+export const userController = {
+    getAllUsers: async(req: Request,res: Response): Promise<void> =>{
+        try {
+           const users = await userModel.selectAllUsers()
+     
+           if(!users.length){
+              res.statusCode = 404
+              throw new Error("No recipes found")
+           }
+     
+           res.status(200).send(users)
+           
+        } catch (error) {
+           console.log(error)
+           res.send(error.message || error.sqlMessage)
+        }
+     },
+     getUsersByName: async(req: Request,res: Response): Promise<void> =>{
+        try {
+           const name = req.query.name
+
+           if (!name) {
+               res.statusCode = 400
+               throw new Error("name not specified")
+           }
+
+           const users = await userModel.selectUsersByName(name as string)
+                      
+           if(!users.length){
+              res.statusCode = 404
+              throw new Error("No recipes found")
+           }
+     
+           res.status(200).send(users)
+           
+        } catch (error) {
+           console.log(error)
+           res.send(error.message || error.sqlMessage)
+        }
+     },
+     getUsersByType: async(req: Request,res: Response): Promise<void> =>{
+        try {
+           const type = req.params.type
+
+           if (!type) {
+               res.statusCode = 400
+               throw new Error("type not specified")
+           }
+
+           const users = await userModel.selectUsersByType(type as string)
+                      
+           if(!users.length){
+              res.statusCode = 404
+              throw new Error("No recipes found")
+           }
+     
+           res.status(200).send(users)
+           
+        } catch (error) {
+           console.log(error)
+           res.send(error.message || error.sqlMessage)
+        }
+     }
+}
---
+++ semana17/aula49/src/model/userModel.ts
@@ -0,0 +1,30 @@
+import { connection } from '../connection';
+
+export const userModel = {
+    selectAllUsers: async (): Promise<any> => {
+        const result = await connection.raw(`
+           SELECT id, name, email, type
+           FROM aula48_exercicio;
+        `)
+     
+        return result[0]
+     },
+     selectUsersByName: async (name: string): Promise<any> => {
+      const result = await connection.raw(`
+         SELECT id, name, email, type
+         FROM aula48_exercicio
+         WHERE name LIKE '%${name}%';
+      `)
+   
+      return result[0]
+   },
+   selectUsersByType: async (type: string): Promise<any> => {
+      const result = await connection.raw(`
+         SELECT id, name, email, type
+         FROM aula48_exercicio
+         WHERE type LIKE '%${type}%';
+      `)
+   
+      return result[0]
+   }
+}
