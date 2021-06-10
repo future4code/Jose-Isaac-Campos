78623cab15c2431ad864040f5c1b7f08ab30a402
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-06-10 16:02:18 -0300

    Criado o ennpoint de login

--- semana18/cookenu/backend/src/controllers/userController.ts
+++ semana18/cookenu/backend/src/controllers/userController.ts
@@ -3,7 +3,7 @@ import { userModel } from '../model/userModel'
 import { user } from '../types'
 import { generateToken } from '../utils/autorizator'
 import { generateId } from '../utils/generateId'
-import { generateHash } from '../utils/hashManager'
+import { compareHash, generateHash } from '../utils/hashManager'
 import { validateEmail } from '../utils/validateEmail'
 import { validatePassword } from '../utils/validatePassword'
 
@@ -44,6 +44,43 @@ export const userController = {
       const result = await userModel.create(newUser)
 
       res.send({access_token: generateToken({id: newUser.id})})
+    } catch (error) {
+      if (res.statusCode === 200) {
+         res.status(500).send({ message: "Internal server error" })
+      } else {
+         res.send({ message: error.message })
+      }
+    }
+  },
+  login: async (req: Request, res: Response) => {
+    try {
+      const { email, password }: user = req.body
+
+      if (!validateEmail(email)) {
+        res.statusCode = 400
+        throw new Error('Sorry, the email format is not valid!')
+      }
+
+      if (!validatePassword(password)) {
+        res.statusCode = 400
+        throw new Error('Sorry, the password must be at least 6 characters long and at least one numeric and one special character.')
+      }
+
+      const [user] = await userModel.findByEmail(email)
+
+      if (!user) {
+        res.statusCode = 404
+        throw new Error('User not found! make sure you typed the email correctly.')
+      }
+
+      const isValidPassword = compareHash(password, user.password)
+
+      if (!isValidPassword) {
+        res.statusCode = 401
+        throw new Error('Invalid password, please try again!')
+      }
+
+      res.send({access_token: generateToken({id: user.id})})
     } catch (error) {
       if (res.statusCode === 200) {
          res.status(500).send({ message: "Internal server error" })
--- semana18/cookenu/backend/src/routes/userRouter.ts
+++ semana18/cookenu/backend/src/routes/userRouter.ts
@@ -4,5 +4,6 @@ import { userController } from '../controllers/userController';
 const RouterUser = express.Router()
 
 RouterUser.post('/signup', userController.create)
+RouterUser.post('/login', userController.login)
 
 export default RouterUser;
