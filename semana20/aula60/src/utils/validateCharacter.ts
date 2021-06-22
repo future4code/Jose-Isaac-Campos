e5a45b1a710b2b9929e2fe9d1093fe5159927218
José Isaac <joseisaacbarreirocampos@gmail.com>
2021-06-22 13:59:16 -0300

    Implementado a interface do personagem e sua função para validação

---
+++ semana20/aula60/src/utils/validateCharacter.ts
@@ -0,0 +1,23 @@
+import { Character } from "../model/Character";
+
+export const validateCharacter = (character: Character) => {
+    const { name, life, defense, attack }: Character = character
+
+    if (!name || !life || !defense || !attack) {
+        return false
+    }
+
+    if (life <= 1500 || typeof life !== "number") {
+        return false
+    }
+
+    if (defense <= 0 || typeof defense !== "number") {
+        return false
+    }
+
+    if (attack <= 0 || typeof attack !== "number") {
+        return false
+    }
+
+    return true
+}
